4ec3e9229e751e0bad22996ca33267ca75cc692f
elisachris1 <elisachris1@gmail.com>
2022-08-19 19:54:02 -0700

    router for technical list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
+import { TechnicianListComponent } from './components/technician-list/technician-list.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: NavComponent, children:[{
-      path:'home', component: HomeComponent
-    }]
+    component: NavComponent, children:[
+      {path:'home', component: HomeComponent},
+      {path: 'technicians', component: TechnicianListComponent}
+    ]
   }
 ];
 
