da56fc05fd5cbc7ce3b87210445d1d64c8e724a2
elisachris1 <elisachris1@gmail.com>
2022-08-20 13:42:55 -0700

    component login created

--- src/app/components/technician-list/technician-list.component.ts
+++ src/app/components/technician-list/technician-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { technician } from 'src/app/models/techinician';
 
 @Component({
   selector: 'app-technician-list',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./technician-list.component.css']
 })
 export class TechnicianListComponent implements OnInit {
-
+  ELEMENT_DATA: technician[] = [
+    {
+      id: 1,
+      name: 'Elisa Rovani',
+      email: 'elisa@email.com',
+      password: '1234',
+      profiles: ['0'],
+      dateCreated: '2022/08/20'
+    }
+  ]
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<technician>(this.ELEMENT_DATA);
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
