91170228bbb7eaa30cbf1102d593d0a3d3b1629e
elisachris1 <elisachris1@gmail.com>
2022-08-20 18:09:25 -0700

    toastr installed and css configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TechnicianListComponent } from './components/technician-list/technician-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
@@ -64,7 +65,12 @@ import { LoginComponent } from './components/login/login.component';
     MatCardModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+   ToastrModule.forRoot({
+    timeOut:4000,
+    closeButton: true,
+    progressBar: true
+   })
   ],
   providers: [],
   bootstrap: [AppComponent]
