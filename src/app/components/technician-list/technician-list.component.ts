19f62f781a2930dc56433ca0b2ab65ac72b34529
elisachris1 <elisachris1@gmail.com>
2022-08-21 16:13:41 -0700

    technical list components finalized

--- src/app/components/technician-list/technician-list.component.ts
+++ src/app/components/technician-list/technician-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { technician } from 'src/app/models/techinician';
+import { TechinicianService } from 'src/app/services/techinician.service';
 
 @Component({
   selector: 'app-technician-list',
@@ -10,27 +11,35 @@ import { technician } from 'src/app/models/techinician';
 })
 export class TechnicianListComponent implements OnInit {
   ELEMENT_DATA: technician[] = [
-    {
-      id: 1,
-      name: 'Elisa Rovani',
-      email: 'elisa@email.com',
-      password: '1234',
-      profiles: ['0'],
-      dateCreated: '2022/08/20'
-    }
+    
   ]
   
   displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource<technician>(this.ELEMENT_DATA);
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private Service: TechinicianService
+  ) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
-
+  findAll(){
+    this.Service.findAll().subscribe(response =>{
+      this.ELEMENT_DATA = response
+      this.dataSource = new MatTableDataSource<technician>(response);
+      this.dataSource.paginator = this.paginator;
+    })
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
---
+++ src/app/services/techinician.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_CONFIG } from '../config/api.config';
+import { technician } from '../models/techinician';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TechinicianService {
+
+  constructor(private http: HttpClient) { }
+
+  findAll(): Observable<technician[]> {
+    return this.http.get<technician[]>(`${API_CONFIG.baseUrl}/technicians`);
+  }
+}
