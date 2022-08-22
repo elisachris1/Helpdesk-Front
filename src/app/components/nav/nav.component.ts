bdb33c084e2cad5374ca2bec460854fa2ee04789
elisachris1 <elisachris1@gmail.com>
2022-08-21 17:57:27 -0700

    HTML and CSS for TECNICIAN-CREATE

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -15,7 +15,7 @@ export class NavComponent implements OnInit {
     private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['home'])
+    this.router.navigate(['technicians/create'])
   }
   logout(){
     this.router.navigate(['login'])
