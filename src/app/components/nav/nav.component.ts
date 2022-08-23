d312de34cf6bc33d57937c7b5d95027e2144efb0
elisachris1 <elisachris1@gmail.com>
2022-08-22 19:53:52 -0700

    btn actions for technician

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -15,7 +15,7 @@ export class NavComponent implements OnInit {
     private toast: ToastrService) { }
 
   ngOnInit(): void {
-    this.router.navigate(['technicians/create'])
+    this.router.navigate(['technicians'])
   }
   logout(){
     this.router.navigate(['login'])
