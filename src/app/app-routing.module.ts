d2036f9ce2c8285efa3764d635a5674f5b76c1f0
elisachris1 <elisachris1@gmail.com>
2022-08-22 21:24:28 -0700

    route to delete component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TechnicianListComponent } from './components/technician-list/technician-list.component';
 import { TechinicianCreateComponent } from './components/technician/techinician-create/techinician-create.component';
+import { TechnicianDeleteComponent } from './components/technician/technician-delete/technician-delete.component';
 import { TechnicianUpdateComponent } from './components/technician/technician-update/technician-update.component';
 
 const routes: Routes = [
@@ -16,7 +17,8 @@ const routes: Routes = [
       {path:'home', component: HomeComponent},
       {path: 'technicians', component: TechnicianListComponent},
       {path: 'technicians/create', component: TechinicianCreateComponent},
-      {path: 'technicians/update/:id', component: TechnicianUpdateComponent}
+      {path: 'technicians/update/:id', component: TechnicianUpdateComponent},
+      {path: 'technicians/delete/:id', component: TechnicianDeleteComponent}
     ]
   }
 ];
