b5dfabe3490e4654fac93d2fc37c478883cd9588
elisachris1 <elisachris1@gmail.com>
2022-08-23 20:12:09 -0700

    call inteface created

---
+++ src/app/models/call.ts
@@ -0,0 +1,15 @@
+export interface Call{
+    
+    id?:                   any;
+    dateCreated?:       string;
+    closeDate?:         string;
+    priority:           String;
+    status:             String;
+    title:              string;
+    description:        string;
+    technician:         string;
+    client:             string;
+    clientName:         string;
+    technicianName:     string;
+    
+}
