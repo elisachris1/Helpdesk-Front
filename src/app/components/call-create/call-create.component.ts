58b42bc981152e4240617e2aa9a79dfda6a91998
elisachris1 <elisachris1@gmail.com>
2022-08-24 12:28:05 -0700

    form validation for call create

--- src/app/components/call-create/call-create.component.ts
+++ src/app/components/call-create/call-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-call-create',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CallCreateComponent implements OnInit {
 
+  priority:     FormControl = new FormControl(null, [Validators.required])
+  status:       FormControl = new FormControl(null, [Validators.required])
+  title:        FormControl = new FormControl(null, [Validators.required])
+  description:  FormControl = new FormControl(null, [Validators.required])
+  technician:   FormControl = new FormControl(null, [Validators.required])
+  client:       FormControl = new FormControl(null, [Validators.required])
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
+
+
+validFields(): boolean{
+  return this.priority.valid && this.status.valid &&
+  this.title.valid && this.description.valid &&
+  this.technician.valid &&  this.client.valid
+}
+ 
 }
